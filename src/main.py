8c603b4424c5c9b610d98a7102c95a872886d9f9
ewokcillo <azotillo@gmail.com>
2022-06-16 19:54:48 +0200

    Add docstring

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 def main():
+    '''
+    this code is to do anything
+    '''
     print("Hello World!")
 
 if __name__ == "__main__":
+    '''
+    this code is to do anything
+    '''
     main()
